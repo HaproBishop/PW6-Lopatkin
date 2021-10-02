13b6cb78b4bba4d86749347b8c11a7a1c5f226fc
HaproBishop <haprobishop@yandex.ru>
2021-10-02 17:06:09 +0300

    Добавьте файлы проекта.

---
+++ LibPair/Pair.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibPair
+{/// <summary>
+/// Класс, производящий умножение пары четных чисел
+/// </summary>
+    public class Pair
+    {        
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+        public int Value3 { get; set; }
+        public int Value4 { get; set; }
+        public int[] Result { get; set; }
+        public Pair() { }                    
+        public Pair(int value1)
+        {
+            Value1 = value1;           
+        }
+        public Pair(int value1, int value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+        public Pair(int value1, int value2, int value3)
+        {
+            Value1 = value1;
+            Value2 = value2;
+            Value3 = value3;
+        }
+        public Pair(int value1, int value2, int value3, int value4)
+        {
+            Value1 = value1;
+            Value2 = value2;
+            Value3 = value3;
+            Value4 = value4;
+        }/// <summary>
+         /// Проверяет на четность числа при созданном экземпляре объекта</summary>
+         /// <returns>true - в случае, если все числа четные, false - в случае, если есть нечетные числа</returns>
+        private bool ProveValuesEvenWithObject()
+        {            
+            if (Value1 % 2 == 0)
+                if (Value2 % 2 == 0)
+                    if (Value3 % 2 == 0)
+                        if (Value4 % 2 == 0) return true;
+            return false;
+        }/// <summary>
+        /// Производит умножение 2 пар чисел при созданном экземпляре объекта
+        /// </summary>
+        /// <returns>Result - массив, где Result[0] - первое произведение, а Result[1] - второе произведение; Иначе - null, если числа не являются четными</returns>
+        public int[] PairCalculateWithObject()
+        {            
+            if (ProveValuesEvenWithObject() == true)
+            {
+                Result = new int[2];
+                Result[0] = Value1 * Value3;
+                Result[1] = Value2 * Value4;
+                return Result;
+            }
+            else return null;
+        }/// <summary>
+         /// Проверяет на четность числа без создания экземпляра объекта
+         /// </summary>
+         /// <param name="value1">Первое число</param>
+         /// <param name="value2">Второе число</param>
+         /// <param name="value3">Третье число</param>
+         /// <param name="value4">Четвертое число</param>
+         /// <returns>true - в случае, если все числа четные, false - в случае, если есть нечетные числа</returns>
+        private static bool ProveValuesEvenWithoutObject(int value1, int value2, int value3, int value4)
+        {
+            if (value1 % 2 == 0)
+                if (value2 % 2 == 0)
+                    if (value3 % 2 == 0)
+                        if (value4 % 2 == 0) return true;
+            return false;
+        }/// <summary>
+         ///  Производит умножение 2 пар чисел без созданного экземпляра объекта
+         /// </summary>
+         /// <param name="value1">Первое число</param>
+         /// <param name="value2">Второе число</param>
+         /// <param name="value3">Третье число</param>
+         /// <param name="value4">Четвертое число</param>
+         /// <returns>null - в случае, если числа не являются четными, mas - в случае, если числа являются четными</returns>
+        public static int[] PairCalculateWithoutObject(int value1, int value2, int value3, int value4)
+        {
+            if (ProveValuesEvenWithoutObject(value1, value2, value3, value4) == true)
+            {
+                int[] mas = new int[2];
+                mas[0] = value1 * value3;
+                mas[1] = value2 * value4;
+                return mas;
+            }
+            else return null;
+        }
+        public static Pair operator ++(Pair value1)
+        {
+            if (value1.Value1 % 2 == 0 && value1.Value2 % 2 == 0)
+                return new Pair { Value1 = value1.Value1 + value1.Value2 };
+            else return null;
+        }
+    }
+}
---
+++ PW5/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibPair;
+/// <summary>
+/// Практическая работа №5. Задание 8. Лопаткин Сергей ИСП-31.
+/// Создать класс Pair (пара четных чисел). Создать необходимые методы и свойства. 
+/// Определить метод вычисления произведения чисел. 
+/// Создать перегруженный метод для вычисления произведения 2 пар чисел - (a,b)*(c,d) = (a*c), (b*d)"
+/// </summary>
+namespace PW5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Support_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Программа имеет следующие особенности:\n1) Необходимо вводить только четные числа для вычисления.\n2) Максимальное число для ввода в полях - пятизначное.\n3) Первое произведение - умножение первого числа на третье. Второе произведение - умножение второго числа на четвертое.\n4) Для вычисления инкременции используются первое и второе числа.", "Справка", MessageBoxButton.OK, MessageBoxImage.Information);            
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AboutProgram_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №6. Задание 8. Использовать класс Pair (пара четных чисел). Разработать операцию инкремента - (a, b) = (a + b, b). Разработать операцию для вычисления произведения 2-х пар чисел - (a, b) * (c, d) = (a * c, b * d)", "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void FirstValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FirstResult.Clear();
+            SecondResult.Clear();
+            if (e.Source != ThirdValue && e.Source != ForthValue)
+                ResultOfIncremention.Clear();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {            
+            bool ProveValue1 = int.TryParse(FirstValue.Text, out int value1);
+            bool ProveValue2 = int.TryParse(SecondValue.Text, out int value2);
+            bool ProveValue3 = int.TryParse(ThirdValue.Text, out int value3);
+            bool ProveValue4 = int.TryParse(ForthValue.Text, out int value4);
+            if (ProveValue1 == true && ProveValue2 == true && ProveValue3 == true && ProveValue4 == true)
+            {
+                int[] mas = Pair.PairCalculateWithoutObject(value1, value2, value3, value4);
+                if (mas != null)
+                {
+                    FirstResult.Text = mas[0].ToString();
+                    SecondResult.Text = mas[1].ToString();
+                }
+                else MessageForUser(false);
+            }
+            else MessageForUser(true);
+        }
+
+        private void FirstValue_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Calculate.IsDefault = false;
+            CalculateIncremention.IsDefault = true;
+        }
+
+        private void SecondValue_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Calculate.IsDefault = false;
+            CalculateIncremention.IsDefault = true;
+        }
+
+        private void ThirdValue_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Calculate.IsDefault = true;
+            CalculateIncremention.IsDefault = false;
+        }
+
+        private void ForthValue_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Calculate.IsDefault = true;
+            CalculateIncremention.IsDefault = false;
+        }
+
+        private void CalculateIncremention_Click(object sender, RoutedEventArgs e)
+        {
+            bool ProveValue1 = int.TryParse(FirstValue.Text, out int value1);
+            bool ProveValue2 = int.TryParse(SecondValue.Text, out int value2);
+            if (ProveValue1 == true && ProveValue2 == true)
+            {
+                Pair firstvalue = new Pair(value1, value2);
+                firstvalue++;
+                if (firstvalue == null) MessageForUser(false);
+                else ResultOfIncremention.Text = firstvalue.Value1.ToString();
+            }
+            else MessageForUser(true);
+        }
+        /// <summary>
+        /// Используется для уведомления пользователя о конкретных ошибках, 
+        /// вызванных неправильным общением с программой
+        /// </summary>
+        /// <param name="isemptystring">isemptystring - значение пустой строки, true - пустая строка, false - не пустая строка</param>
+        public static void MessageForUser(bool isemptystring)
+        {
+            if(isemptystring == true)
+                MessageBox.Show("У вас некорректно введены значения для проведения произведения! Подробности об особенностях работы программы написаны в справке!", "ОШИБКА!", MessageBoxButton.OK, MessageBoxImage.Error);
+            else MessageBox.Show("У вас введены нечетные(ое) числа(о)! Пожалуйства, введите значения в соответствии с требованиями!", "ОШИБКА!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
