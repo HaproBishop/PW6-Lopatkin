eaf7ab795ba5ba2a024e60aadbe8b34777028bf5
Hapro <acfowo45@gmail.com>
2021-10-24 19:52:35 +0300

    Ключевые изменения: - Взяты наработки класса с предыдущей работы - Поправлен весь код под новый класс Pair

--- PW5/MainWindow.xaml.cs
+++ PW5/MainWindow.xaml.cs
@@ -30,7 +30,9 @@ namespace PW5
         {
             InitializeComponent();
         }
-
+        Pair result = new Pair();
+        Pair firstpair = new Pair();
+        Pair secondpair = new Pair();
         private void Support_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Программа имеет следующие особенности:\n1) Необходимо вводить только четные числа для вычисления.\n2) Максимальное число для ввода в полях - пятизначное.\n3) Первое произведение - умножение первого числа на третье. Второе произведение - умножение второго числа на четвертое.\n4) Для вычисления инкременции используются первое и второе числа.", "Справка", MessageBoxButton.OK, MessageBoxImage.Information);            
@@ -38,44 +40,50 @@ namespace PW5
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void AboutProgram_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Практическая работа №6. Задание 8. Использовать класс Pair (пара четных чисел). Разработать операцию инкремента - (a, b) = (a + b, b). Разработать операцию для вычисления произведения 2-х пар чисел - (a, b) * (c, d) = (a * c, b * d)", "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
         }
-
+        /// <summary>
+        /// Используется для очистки результатов
+        /// при изменении начальных значений
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void FirstValue_TextChanged(object sender, TextChangedEventArgs e)
         {
             FirstResult.Clear();
             SecondResult.Clear();
-            if (e.Source != ThirdValue && e.Source != ForthValue)
+            if (e.Source != ThirdValue && e.Source != ForthValue) //Проверка на очистку результата при изменении 1 или 2 строки
                 ResultOfIncremention.Clear();
         }
 
         private void Calculate_Click(object sender, RoutedEventArgs e)
-        {            
+        {
             bool ProveValue1 = int.TryParse(FirstValue.Text, out int value1);
             bool ProveValue2 = int.TryParse(SecondValue.Text, out int value2);
             bool ProveValue3 = int.TryParse(ThirdValue.Text, out int value3);
             bool ProveValue4 = int.TryParse(ForthValue.Text, out int value4);
             if (ProveValue1 == true && ProveValue2 == true && ProveValue3 == true && ProveValue4 == true)
             {
-                Pair result = new Pair(value1, value2);
-                Pair pairobject = new Pair(value3, value4);
-                result.PairCalculate(pairobject);
-                if (result.Result != null)
-                {
-                    FirstResult.Text = result.Result[0].ToString();
-                    SecondResult.Text = result.Result[1].ToString();
-                }
-                else MessageForUser(false);
+                firstpair.AddValue(value1, value2);
+                secondpair.AddValue(value3, value4);
+                result = firstpair.PairCalculate(secondpair);//Занесение результатов в объект result
+                FirstResult.Text = result.Value1.ToString();//Вывод результатов
+                SecondResult.Text = result.Value2.ToString();
             }
-            else MessageForUser(true);
+            else MessageForUser();
         }
-
-        private void FirstValue_GotFocus(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Изменение дефолтного Enter при фокусировании на разных TextBox'ах
+        /// Используется для удобного переключения посредством Tab и нажатия Enter после ввода значений
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FirstValue_GotFocus(object sender, RoutedEventArgs e) 
         {
             if (e.Source == FirstValue || e.Source == SecondValue)
             {
@@ -95,24 +103,18 @@ namespace PW5
             bool ProveValue2 = int.TryParse(SecondValue.Text, out int value2);
             if (ProveValue1 == true && ProveValue2 == true)
             {
-                Pair firstvalue = new Pair(value1, value2);
-                firstvalue++;
-                if (firstvalue == null) MessageForUser(false);
-                else ResultOfIncremention.Text = firstvalue.Value1.ToString();
+                firstpair.AddValue(value1, value2);
+                result = ++firstpair;
+                ResultOfIncremention.Text = result.Value1.ToString();
             }
-            else MessageForUser(true);
+            else MessageForUser();
         }
         /// <summary>
-        /// Используется для уведомления пользователя о конкретных ошибках, 
-        /// вызванных неправильным общением с программой
+        /// Метод для сообщения пользователю об некорректности значений, введенных им
         /// </summary>
-        /// <param name="isemptystring">isemptystring - значение пустой строки, true - пустая строка, false - не пустая строка</param>
-        public static void MessageForUser(bool isemptystring)
+        public void MessageForUser()
         {
-            if(isemptystring == true)
-                MessageBox.Show("У вас некорректно введены значения для проведения произведения! Подробности об особенностях работы программы написаны в справке!", "ОШИБКА!", MessageBoxButton.OK, MessageBoxImage.Error);
-            else 
-                MessageBox.Show("У вас введены нечетные(ое) числа(о)! Пожалуйства, введите значения в соответствии с требованиями!", "ОШИБКА!", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
+            MessageBox.Show(Pair.InfoUser, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }        
     }
 }
